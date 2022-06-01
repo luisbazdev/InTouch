c3226a1f7df0570aa0d0023e603ae2736ff78f8b
luisbazdev <luisbazd@gmail.com>
2022-06-01 12:03:01 -0400

    Create branches section layout

--- next/components/layout/Branch.js
+++ next/components/layout/Branch.js
@@ -2,6 +2,9 @@ import styles from './Branch.module.css'
 
 export default function Branch({branch}){
     return (
-        <div>{branch.data().branch}</div>
+        // <div>{branch.data().branch}</div>
+        <div className={styles.branch}>
+            <p style={{color: branch.data().color}}>@{branch.data().branch}</p>
+        </div>
     )
 }
